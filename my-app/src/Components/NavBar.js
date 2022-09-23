af66cf2c807d33bc7adcb6b8b84b5b78691e2b50
Rhodan Cervantes <105742277+rhodanrsc@users.noreply.github.com>
2022-09-23 14:47:55 -0600

    Added conditional rendering for NavBar     Doesn't quite work Added shared folder for css and images

---
+++ my-app/src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import UserNavBar from "./userNavBar.component";
+import NonUserNavBar from "./NonUserNavBar.component";
+
+const isLoggedIn = false;
+
+const NavBar = () => { 
+    
+    if (isLoggedIn) {
+        return <UserNavBar />;
+    } 
+    return <NonUserNavBar />;
+    
+};
+
+export default NavBar;
+
+// Not fully implemented yet.
+// Need to figure out how to know if someone is logged in
