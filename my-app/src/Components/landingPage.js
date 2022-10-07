a27b7e9e37e6c6706697ee9a1ec0eab03e85c904
JShenLee <92648031+JShenLee@users.noreply.github.com>
2022-10-06 20:12:04 -0600

    Added method for accessing user session data in the front end. Added rendering example to the landing page

--- my-app/src/Components/landingPage.js
+++ my-app/src/Components/landingPage.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from '../shared/images/CCLogo.png';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import axios from 'axios';
 const LandingPage = () => {
+//////////////////////////////////////////////////////////////
+// Pulling req.user and setting it to the state variable 'data'
+//////////////////////////////////////////////////////////////
+const [data, setData] = useState(null);
+const getUser = () => {
+axios({
+  method: "get",
+  withCredentials: true,
+  url: "http://localhost:5000/user/getUser",
+}).then((res) => {
+  setData(res.data);
+  console.log(res.data);
+}).catch((err) => alert("Something went wrong: " + err));
+};
+//runs getUser once when the page loads to set state variable data = req.user. 
+useEffect(() => {
+getUser();
+}, [""]);
+//////////////////////////////////////////////////////////////   
+
     return(
         <div className='landingPage'>
             <div className='bannerArea'>
+            {data?<h1>Example of rendering the user object data after logging in.</h1>: null}
+            {data?<h2>username: {data.username} </h2>: null}
+            {data?<h2>_id: {data._id} </h2>: null}
+            {data?<h2>email: {data.email} </h2>: null}
+            {data?<h2>createdAt: {data.createdAt} </h2>: null}
                 <img className='bigLogo' src={logo}/>
                 <h3>Join Clean Collective</h3>
                 <p>Get more features and privileges by joining the most helpful community</p>
