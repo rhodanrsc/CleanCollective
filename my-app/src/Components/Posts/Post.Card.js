2146b859958817277f58199d6ffbe76439be9711
Paignton <paigntonwild@hotmail.com>
2022-10-25 22:46:46 -0600

    updates boiler code to implement Posts

--- my-app/src/Components/Posts/Post.Card.js
+++ my-app/src/Components/Posts/Post.Card.js
@@ -14,6 +14,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import axios from 'axios';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -26,7 +27,14 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function RecipeReviewCard() {
+//Get the User Post Data
+function getUserPost (){
+  console.log("like button clicked");
+  axios.get('/user.post.route').then(() => {console.log('User post data pulled form DB');}).catch(() => {alert("Error pulling user post data");
+});
+}
+
+export default function PostCard() {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -51,6 +59,7 @@ export default function RecipeReviewCard() {
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
+          
           This impressive paella is a perfect party dish and a fun meal to cook
           together with your guests. Add 1 cup of frozen peas along with the mussels,
           if you like.
@@ -58,12 +67,14 @@ export default function RecipeReviewCard() {
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+          <FavoriteIcon/>
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
+        <button onClick={getUserPost}>Get user Post</button>
       </CardActions>
+      
     </Card>
   );
 }
