7e2d211b312de50a8d8cef92877fa65ce8e4c198
Hayley Mead <Hayleymead3@gmail.com>
2022-11-05 19:24:34 -0600

    small profile icon

--- my-app/src/Components/userProfile/public_profile_Page/middle_panel/information_edit_box.component.js
+++ my-app/src/Components/userProfile/public_profile_Page/middle_panel/information_edit_box.component.js
@@ -1,49 +1,60 @@
 import { ReactSession } from "react-client-session";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //material ui imports
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 //side menu
 import CustomizedMenus from "./user_description_menu.component";
 
+//profile pic
+import BackgroundLetterAvatarsSmall from "./small_user_profile.component";
+
 export default function DescriptionBox() {
   //grabbing user session data
+ 
   let data = ReactSession.get("userSession");
 
-  const [value, setValue] = useState(""); // ADD USER SESSION FOR ABOUT 
+  const [value, setValue] = useState(""); // ADD USER SESSION FOR ABOUT
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
   return (
     <div>
-      <Box
-        component="form"
-        sx={{ "& .MuiTextField-root": { m: 1, width: "40ch" } }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="outlined-multiline-flexible"
-          label="About"
-          multiline
-          maxRows={20}
-          value={value}
-          onChange={handleChange}
-          disabled
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{ "& .MuiTextField-root": { m: 1, width: "40ch" } }}
-        noValidate
-        autoComplete="off"
-      ></Box>
+      <Paper>
       <CustomizedMenus></CustomizedMenus>
+      <BackgroundLetterAvatarsSmall></BackgroundLetterAvatarsSmall>
+      
+        <Box
+          component="form"
+          sx={{ "& .MuiTextField-root": { m: 1, width: "40ch" } }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-multiline-flexible"
+            label="About"
+            multiline
+            maxRows={20}
+            value={value}
+            onChange={handleChange}
+            disabled
+          />
+        </Box>
+        <Box
+          component="form"
+          sx={{ "& .MuiTextField-root": { m: 1, width: "40ch" } }}
+          noValidate
+          autoComplete="off"
+        ></Box>
+        
+      </Paper>
     </div>
   );
 }
