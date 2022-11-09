ff6b33d2c57cac647d9f6bf1e087240d9a5e6d2e
Paignton <paigntonwild@hotmail.com>
2022-11-09 09:53:43 -0700

    Updates comment model to include parent fields

--- my-app/backend/models/user.comments.js
+++ my-app/backend/models/user.comments.js
@@ -5,19 +5,31 @@ const Schema = mongoose.Schema;
 
 const userCommentSchema = new Schema(
   {
+    // Use Mongo DB generated id 
+    // Stores username of the person who wrote the comment
+    commentUsername: {
+      type: String,
+    },
+    // stores user that wrote the comment
     commentUserId: {
       type: String,
       required: false,
+    },
+    // postId that the comment is written on
+    commentPostId: {
+      type: String,
     },
     commentBody: {
       type: String,
       required: false,
     },
-    commentParentId: { //used to handle reply messages
+    // used to store the ID that the comment is replying to 
+    commentParentId: { 
       type: String,
       required: false,
     },
   },
+  // generates createdAt and updatedAt fields which store an ISO string
   {
     timestamps: true,
   }
