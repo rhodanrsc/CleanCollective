1e7b288b36f864c908790d8b2754e00ef52ea420
JShenLee <92648031+JShenLee@users.noreply.github.com>
2022-11-18 15:25:28 -0700

    main post page error fix

--- my-app/src/Components/Posts/main-post-page.js
+++ my-app/src/Components/Posts/main-post-page.js
@@ -76,9 +76,17 @@ export class PostPage extends Component {
         key={post._id}
           />
     
-
-        
-
+      )) : this.state.posts.reverse().map((post) => (
+         
+        <PostCard
+        id={post._id}
+        username={post.postUserName}
+        title={post.postTitle}
+        body={post.postBody}
+        likes={post.postLikes}
+        key={post._id}
+          />
+      
       ))}
 
       </div>
