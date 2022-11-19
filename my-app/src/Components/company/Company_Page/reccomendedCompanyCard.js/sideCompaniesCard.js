12d92ed1b3de188c0836cf5d6d635a58e3399ac4
JimbobCS <jvan709@gmail.com>
2022-11-19 11:33:25 -0700

    Side company reccomended panel done

--- my-app/src/Components/company/Company_Page/reccomendedCompanyCard.js/sideCompaniesCard.js
+++ my-app/src/Components/company/Company_Page/reccomendedCompanyCard.js/sideCompaniesCard.js
@@ -1,11 +1,53 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Box, Grid, Card, CardHeader, CardContent, Avatar, IconButton, Typography, Button, ButtonGroup, Chip } from "@mui/material"
+import { useNavigate } from "react-router-dom";
 
 export default function SideCompaniesCard() {
-
+    const navigate = useNavigate();
     const [companies, setCompanies] = useState([])
-    const [chipState, setChipState] = useState("outlined")
+    let chipState;
+    const [chipState0, setChipState0] = useState("outlined")
+    const [chipState1, setChipState1] = useState("outlined")
+    const [chipState2, setChipState2] = useState("outlined")
+
+    const handleChipFill = (event) => {
+        let hoveredChip = event.target.id
+
+
+        if (hoveredChip === "chip0") {
+            setChipState0("contained")
+        } else if (hoveredChip === "chip1") {
+            setChipState1("contained")
+        } else if (hoveredChip === "chip2") {
+            setChipState2("contained")
+        }
+    }
+
+    const handleChipUnFill = (event) => {
+        let hoveredChip = event.target.id
+        if (hoveredChip === "chip0") {
+            setChipState0("outlined")
+        } else if (hoveredChip === "chip1") {
+            setChipState1("outlined")
+        } else if (hoveredChip === "chip2") {
+            setChipState2("outlined")
+        }
+    }
+
+    const checkChipNumber = (index) => {
+        if (index === 0) {
+            return chipState0
+        } else if (index === 1) {
+            return chipState1
+        } else {
+            return chipState2
+        }
+    }
+
+    const handleChipClick = (event, thisCompanyName) => {
+        navigate('/companyPage/' + thisCompanyName);
+    }
 
 
 
@@ -16,7 +58,7 @@ export default function SideCompaniesCard() {
                 let allCompanies = response.data;
                 listOfCompanies.push(allCompanies[0])
                 listOfCompanies.push(allCompanies[1])
-                listOfCompanies.push(allCompanies[2])
+                listOfCompanies.push(allCompanies[3])
                 setCompanies(listOfCompanies)
             })
             .catch((error) => console.log("Error with getting companie: " + error))
@@ -31,9 +73,9 @@ export default function SideCompaniesCard() {
                     title="Related Companies"
                 />
                 <CardContent>
-                    {companies.map((company) => {
+                    {companies.map((company, index) => {
                         return (
-                            <Card style={{ marginTop: "7px", marginBottom: "7px" }} >
+                            <Card key={company.companyName + "Card"} style={{ marginTop: "7px", marginBottom: "7px" }} >
                                 <CardHeader
                                     key={company.companyName}
                                     avatar={
@@ -47,9 +89,16 @@ export default function SideCompaniesCard() {
                                 />
 
                                 <Chip
+                                    key={company.companyName + "" + index}
+
+                                    onMouseEnter={handleChipFill}
+                                    onMouseLeave={handleChipUnFill}
+                                    onClick={event => handleChipClick(event, company.companyName)}
+
+                                    id={"chip" + index}
                                     style={{ margin: "0px 0 10px 68px" }}
                                     component={'span'}
-                                    variant={chipState}
+                                    variant={checkChipNumber(index)}
                                     label="View Profile"
                                     color="primary"
 
