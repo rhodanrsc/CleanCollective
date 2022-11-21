c3d5e5b8b93b508f208e10656a5fe2cc3cd69fdb
Bluenurples <andy.diep@edu.sait.ca>
2022-11-20 18:00:25 -0700

    The comment input box is now able to take in multiline comments and move to next line when end of box length has been reached.

--- my-app/src/Components/Comments/PCommentForm.js
+++ my-app/src/Components/Comments/PCommentForm.js
@@ -43,11 +43,13 @@ const PCommentForm = (props) => {
   return (
     <form onSubmit={onSubmit}>
       <TextField label="Comments" id="fullWidth" 
+        multiline
         className="comment-form-textarea"
         value={text}
-        style= {{width:"550px", maxWidth: "100%", flex: 1, flexWrap: 'wrap', wordWrap:"break-word", wordBreak:"break-word", overflowX: "hidden", overflowWrap:"anywhere", marginLeft:"65px", paddingLeft:"20px", paddingRight:"0px", paddingTop:"5px",marginBottom:"10px", marginTop: "10px", borderRadius:"0.2em", border:"none"}}
+        style= {{width:"550px", maxWidth: "100%", flex: 1, flexWrap: 'wrap', wordWrap:"break-word", wordBreak:"break-word", overflowWrap:"anywhere", marginLeft:"65px", paddingLeft:"20px", paddingRight:"0px", paddingTop:"5px",marginBottom:"10px", marginTop: "10px", borderRadius:"0.2em", border:"none"}}
         onChange={(e) => setText(e.target.value)}
         cols={60}
+        
       />
       <Button onClick={onSubmit} className="comment-form-button" disabled={isTextareaDisabled} style = {{marginLeft:"480px"}}>
         Enter
