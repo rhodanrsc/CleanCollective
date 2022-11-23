377ecb10fea2458a812782424eb32abed33afdc3
Bluenurples <andy.diep@edu.sait.ca>
2022-11-23 14:57:22 -0700

    Changed sizing of comment box

--- my-app/src/Components/Comments/PCommentForm.js
+++ my-app/src/Components/Comments/PCommentForm.js
@@ -46,12 +46,12 @@ const PCommentForm = (props) => {
         multiline
         className="comment-form-textarea"
         value={text}
-        style= {{width:"550px", maxWidth: "100%", flex: 1, flexWrap: 'wrap', wordWrap:"break-word", wordBreak:"break-word", overflowWrap:"anywhere", marginLeft:"65px", paddingLeft:"20px", paddingRight:"0px", paddingTop:"5px",marginBottom:"10px", marginTop: "10px", borderRadius:"0.2em", border:"none"}}
+        style= {{width:"550px", maxWidth: "100%", flex: 1, flexWrap: 'wrap', wordWrap:"break-word", wordBreak:"break-word", overflowWrap:"anywhere", marginLeft:"80px", paddingLeft:"20px", paddingRight:"0px", paddingTop:"5px",marginBottom:"10px", marginTop: "10px", borderRadius:"0.2em", border:"none"}}
         onChange={(e) => setText(e.target.value)}
         cols={60}
         
       />
-      <Button onClick={onSubmit} className="comment-form-button" disabled={isTextareaDisabled} style = {{marginLeft:"480px"}}>
+      <Button onClick={onSubmit} className="comment-form-button" disabled={isTextareaDisabled} style = {{marginLeft:"495px"}}>
         Enter
       </Button>
         <Button
